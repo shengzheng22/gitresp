7e3a4d765ff54af546474cb318aaf1edd794ee55
shengzheng <shengzheng@nblnet.cn>
2022-06-15 16:31:21 +0800

    增加了com.web下的主方法

---
+++ demoweb/src/main/java/com/web/demo.java
@@ -0,0 +1,13 @@
+package com.web;
+
+/**
+ * @Auther: sz
+ * @Date: 2022/6/15 - 06 - 15 - 16:30
+ * @Description: com.web
+ * @version: 1.0
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello demo");
+    }
+}
