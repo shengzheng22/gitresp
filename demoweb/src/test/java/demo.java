517dd08b23a92b74464d8a100d2ff446c0f5b72d
shengzheng <shengzheng@nblnet.cn>
2022-06-15 16:29:06 +0800

    增加了test测试类

---
+++ demoweb/src/test/java/demo.java
@@ -0,0 +1,12 @@
+/**
+ * @Auther: sz
+ * @Date: 2022/6/15 - 06 - 15 - 16:04
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("1111111");
+    }
+}
